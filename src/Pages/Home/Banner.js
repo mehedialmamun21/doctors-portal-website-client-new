a039c32c495a3712bdf47a324a446b673a996cc7
mehedialmamun21 <mehedialmamuncse@gmail.com>
2022-05-11 17:12:45 +0600

    home page banner part completed with background image

---
+++ src/Pages/Home/Banner.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import chair from '../../assets/images/chair.png';
+import img from '../../assets/images/bg.png';
+
+const Banner = () => {
+    return (
+        <div class="hero min-h-screen px-12 bg-[url('/src/assets/images/bg.png')]">
+            <div class="hero-content flex-col lg:flex-row-reverse">
+                <img src={chair} class="max-w-sm rounded-lg shadow-2xl ml-7" />
+                <div>
+                    <h1 class="text-5xl font-bold">Your New Smile Starts Here</h1>
+                    <p class="py-6">Our team is filled with experienced professionals who
+                        are all dedicated to your continued dental health. It is our mission to
+                        keep every patient educated and comfortable during their visits, and we
+                        will do everything we can to streamline your experience and leave you
+                        feeling refreshed and relaxed. Whether you're getting a cleaning, a root
+                        canal, or simply require help understanding your insurance, we're here for
+                        you!</p>
+                    <button class="btn btn-primary uppercase text-white font-bold bg-gradient-to-r from-secondary to-primary">Get Started</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        // 'hero-pattern': "url('/img/hero-pattern.svg')",
+        'footer-texture': "url('/src/assets/images/bg.png')",
+      }
+    },
   },
 
   daisyui: {
