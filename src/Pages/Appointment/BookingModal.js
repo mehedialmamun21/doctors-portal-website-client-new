85139406a2c99a9c01fda1939e77c234266036cb
mehedialmamun21 <mehedialmamuncse@gmail.com>
2022-05-14 16:25:59 +0600

    some functionality added of error handling in console

--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -28,12 +28,16 @@ const BookingModal = ({ date, treatment, setTreatment }) => {
                         <input type="text" disabled value={format(date, 'PP')} className="input input-bordered w-full max-w-xs" />
                         <select name='slot' className="select select-bordered w-full max-w-xs">
                             {
-                                slots.map(slot => <option value={slot}>{slot}</option>)
+                                slots.map((slot, index) => <option
+                                    key={index}
+                                    value={slot}
+
+                                >{slot}</option>)
                             }
                         </select>
-                        <input type="text" name='name' disabled value={user?.displayName} placeholder="Full Name" className="input input-bordered w-full max-w-xs" />
+                        <input type="text" name='name' disabled value={user?.displayName || ''} className="input input-bordered w-full max-w-xs" />
+                        <input type="email" name='email' disabled value={user?.email || ''} className="input input-bordered w-full max-w-xs" />
                         <input type="number" name='phone' placeholder="Phone Number" className="input input-bordered w-full max-w-xs" />
-                        <input type="email" name='email' placeholder="Email" className="input input-bordered w-full max-w-xs" />
                         <input type="submit" value="Submit" className="btn btn-secondary w-full max-w-xs" />
                     </form>
                 </div>
