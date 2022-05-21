d236af3b71a5edf3ef988703040272de89ae22cf
mehedialmamun21 <mehedialmamuncse@gmail.com>
2022-05-22 05:02:36 +0600

    some style added to pay button

--- src/Pages/Dashboard/CheckoutForm.js
+++ src/Pages/Dashboard/CheckoutForm.js
@@ -1,11 +1,24 @@
-import { CardElement, useStripe } from '@stripe/react-stripe-js';
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
 import React from 'react';
 
 const CheckoutForm = () => {
     const stripe = useStripe();
-    const handleSubmit = event => {
+    const elements = useElements()
+
+    const handleSubmit = async (event) => {
         event.preventDefault();
+
+        if (!stripe || !elements) {
+            return;
+        }
+
+        const card = elements.getElement(CardElement);
+
+        if (card == null) {
+            return;
+        }
     }
+
     return (
         <form onSubmit={handleSubmit}>
             <CardElement
@@ -24,7 +37,7 @@ const CheckoutForm = () => {
                     },
                 }}
             />
-            <button type="submit" disabled={!stripe}>
+            <button className='btn btn-success btn-sm mt-4 text-white' type="submit" disabled={!stripe}>
                 Pay
             </button>
         </form>
