9787fcc46941bdc2c3e2be30f49ebd1aa0362418
mehedialmamun21 <mehedialmamuncse@gmail.com>
2022-05-29 23:17:07 +0600

    payment intent section done successfully 77-6

--- src/Pages/Dashboard/__payment_steps.js
+++ src/Pages/Dashboard/__payment_steps.js
@@ -5,7 +5,8 @@
  * 4. create elements wrapper using publishable key
  * 5. create checkout form using Card element, useStripe, useElements
  * 6. get card elements info (credit card info)
- * --------------------
- * 7. get credit card info/error
- * 8. 
+ * 7. get credit card info/error + display card error (if any)
+ * -----------------------------------
+ * 8. get client secret from backend via payment intent post api
+ * 9. store client secret on the client side
 */
