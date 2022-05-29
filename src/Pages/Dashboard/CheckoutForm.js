6f4c38780b7c4d352e2506f8dfb45a5b02125807
mehedialmamun21 <mehedialmamuncse@gmail.com>
2022-05-29 19:00:13 +0600

    payment section error handling done

--- src/Pages/Dashboard/CheckoutForm.js
+++ src/Pages/Dashboard/CheckoutForm.js
@@ -1,9 +1,11 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
 import React from 'react';
+import { useState } from 'react';
 
 const CheckoutForm = () => {
     const stripe = useStripe();
-    const elements = useElements()
+    const elements = useElements();
+    const [cardError, setCardError] = useState('');
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -17,30 +19,42 @@ const CheckoutForm = () => {
         if (card == null) {
             return;
         }
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card
+        });
+
+        setCardError(error?.message || '');
+
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <CardElement
-                options={{
-                    style: {
-                        base: {
-                            fontSize: '16px',
-                            color: '#424770',
-                            '::placeholder': {
-                                color: '#aab7c4',
+        <>
+            <form onSubmit={handleSubmit}>
+                <CardElement
+                    options={{
+                        style: {
+                            base: {
+                                fontSize: '16px',
+                                color: '#424770',
+                                '::placeholder': {
+                                    color: '#aab7c4',
+                                },
+                            },
+                            invalid: {
+                                color: '#9e2146',
                             },
                         },
-                        invalid: {
-                            color: '#9e2146',
-                        },
-                    },
-                }}
-            />
-            <button className='btn btn-success btn-sm mt-4 text-white' type="submit" disabled={!stripe}>
-                Pay
-            </button>
-        </form>
+                    }}
+                />
+                <button className='btn btn-success btn-sm mt-4 text-white' type="submit" disabled={!stripe}>
+                    Pay
+                </button>
+            </form>
+            {
+                cardError && <p className='text-red-500'>{cardError}</p>
+            }
+        </>
     );
 };
 
---
+++ src/Pages/Dashboard/__payment_steps.js
@@ -0,0 +1,11 @@
+/**
+ * 1. install stripe, react stripe js
+ * 2. open stripe account on stripe website
+ * 3. get publishable key pk___
+ * 4. create elements wrapper using publishable key
+ * 5. create checkout form using Card element, useStripe, useElements
+ * 6. get card elements info (credit card info)
+ * --------------------
+ * 7. get credit card info/error
+ * 8. 
+*/
