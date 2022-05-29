223201516fbf537c0306ab0293723ee855a71d7b
mehedialmamun21 <mehedialmamuncse@gmail.com>
2022-05-29 17:11:03 +0600

    home page dinamically added users review

--- src/Pages/Dashboard/MyReview.js
+++ src/Pages/Dashboard/MyReview.js
@@ -1,9 +1,33 @@
 import React from 'react';
-
+import { useForm } from 'react-hook-form';
 const MyReview = () => {
+
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = data => {
+        const url = "http://localhost:5000/review";
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+
+        })
+            .then(res => res.json())
+            .then(result => {
+                console.log(result);
+            })
+    };
+
+
     return (
-        <div>
-            <h2>My review</h2>
+        <div className='addItem w-50 mx-auto mt-10'>
+            <form className='flex flex-col items-center' onSubmit={handleSubmit(onSubmit)}>
+                <input className='input input-bordered w-full max-w-xs my-2' required placeholder="Give us Ratings (1-5).." type="number" {...register("rating")} />
+                <textarea className='input input-bordered w-full max-w-xs my-2' required placeholder="Share your valuable feedback.." {...register("description")} />
+                <input className="btn w-full max-w-xs" type="submit" value="Add Review" />
+            </form>
         </div>
     );
 };
--- src/Pages/Home/Review.js
+++ src/Pages/Home/Review.js
@@ -1,21 +1,12 @@
 import React from 'react';
 
 const Review = ({ review }) => {
+    const { _id, description, rating } = review;
     return (
-        <div className="card lg:max-w-lg bg-base-100 shadow-xl">
-            <div className="card-body">
-                <p>{review.review}</p>
-                <div className='flex items-center'>
-                    <div className="avatar">
-                        <div className="w-16 rounded-full ring ring-primary ring-offset-base-100 mr-5 mt-5">
-                            <img src={review.img} alt="" />
-                        </div>
-                    </div>
-                    <div>
-                        <h4 className='text-xl'>{review.name}</h4>
-                        <h2>{review.location}</h2>
-                    </div>
-                </div>
+        <div class="card lg:card-side bg-base-100 shadow-xl">
+            <div class="card-body">
+                <p> Rating : <span className='text-warning font-bold text-lg'>{rating}</span></p>
+                <p> Review : <span className='text-primary text-lg font-semibold'>{description}</span> </p>
             </div>
         </div>
     );
