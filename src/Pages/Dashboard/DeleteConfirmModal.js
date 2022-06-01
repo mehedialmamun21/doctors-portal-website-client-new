9831ee934f1c45c76c7e01046700f17d17d91ffb
mehedialmamun21 <mehedialmamuncse@gmail.com>
2022-06-01 21:40:32 +0600

    links are replaced by heroku links

--- src/Pages/Dashboard/DeleteConfirmModal.js
+++ src/Pages/Dashboard/DeleteConfirmModal.js
@@ -6,7 +6,7 @@ const DeleteConfirmModal = ({ deletingDoctor, refetch, setDeletingDoctor }) => {
 
 
     const handleDelete = () => {
-        fetch(`http://localhost:5000/doctor/${email}`, {
+        fetch(`https://glacial-bayou-47586.herokuapp.com/doctor/${email}`, {
             method: 'DELETE',
             headers: {
                 authorization: `Bearer ${localStorage.getItem('accessToken')}`
--- src/Pages/Home/Testimonial.js
+++ src/Pages/Home/Testimonial.js
@@ -8,7 +8,7 @@ const Testimonial = () => {
     const [reviews, setReviews] = useState([]);
 
     useEffect(() => {
-        fetch("http://localhost:5000/review")
+        fetch("https://glacial-bayou-47586.herokuapp.com/review")
             .then((res) => res.json())
             .then((data) => setReviews(data));
     }, []);
