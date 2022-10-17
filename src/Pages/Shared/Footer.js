68ea5c7e7f24843d678a67c8be2829f77e669c0b
mehedialmamun21 <mehedialmamuncse@gmail.com>
2022-10-17 18:40:49 +0600

    display date section done

--- src/Pages/Shared/Footer.js
+++ src/Pages/Shared/Footer.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import footerImg from '../../assets/images/footer.png';
 
 const Footer = () => {
-
+    var today = new Date();
+    var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
 };
 
 export default Footer;
