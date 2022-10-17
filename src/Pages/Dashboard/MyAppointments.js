e34e9e25df951d431f9b620eec275bef9ea3939b
mehedialmamun21 <mehedialmamuncse@gmail.com>
2022-10-17 17:34:20 +0600

    My appointment section added with changes

--- src/Pages/Dashboard/MyAppointments.js
+++ src/Pages/Dashboard/MyAppointments.js
@@ -6,75 +6,7 @@ import auth from '../../firebase.init';
 
 const MyAppointments = () => {
 
-    const [appointments, setAppointments] = useState([]);
-    const [user] = useAuthState(auth);
-    const navigate = useNavigate();
 
-    useEffect(() => {
-        if (user) {
-            fetch(`https://limitless-inlet-88208.herokuapp.com/booking?patient=${user.email}`, {
-                method: 'GET',
-                headers: {
-                    'authorization': `Bearer ${localStorage.getItem('accessToken')}`
-                }
-            })
-                .then(res => {
-                    console.log('res', res);
-                    if (res.status === 401 || res.status === 403) {
-                        signOut(auth);
-                        localStorage.removeItem('accessToken');
-                        navigate('/');
-                    }
-                    return res.json()
-                })
-                .then(data => {
-
-                    setAppointments(data);
-
-                });
-        }
-    }, [user])
-
-    return (
-        <div>
-            <h2 className='my-5 text-xl text-center font-bold text-primary'>Your Appointments are : {appointments.length}</h2>
-
-            <div class="overflow-x-auto">
-                <table class="table w-full">
-                    <thead>
-                        <tr>
-                            <th></th>
-                            <th>Name</th>
-                            <th>Date</th>
-                            <th>Time</th>
-                            <th>Treatment</th>
-                            <th>Payment</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            appointments.map((a, index) => <tr key={a._id}>
-                                <th>{index + 1}</th>
-                                <td>{a.patientName}</td>
-                                <td>{a.date}</td>
-                                <td>{a.slot}</td>
-                                <td>{a.treatment}</td>
-                                <td>
-                                    {(a.price && !a.paid) && <Link to={`/dashboard/payment/${a._id}`}> <button className='btn btn-sm btn-success text-white px-5 font-bold text-sm'>Pay</button> </Link>}
-                                    {(a.price && a.paid) && <div>
-                                        <p><span className='text-success font-bold text-xl'>Paid</span></p>
-                                        <p ><span className='font-bold text-sm text-stone-500'>TransactionID : </span><span className='text-warning text-sm font-bold'>{a.transactionId}</span> </p>
-                                    </div>}
-                                </td>
-                            </tr>)
-                        }
-
-                    </tbody>
-                </table>
-            </div>
-
-        </div>
-    );
 };
 
 export default MyAppointments;
