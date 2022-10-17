2e0cef320b52e0ef9a25e562039e8f0a7c551556
mehedialmamun21 <mehedialmamuncse@gmail.com>
2022-10-17 17:54:49 +0600

    users part added in dashboard with associate table

--- src/Pages/Dashboard/Users.js
+++ src/Pages/Dashboard/Users.js
@@ -10,34 +10,17 @@ const Users = () => {
             authorization: `Bearer ${localStorage.getItem('accessToken')}`
         }
     }).then(res => res.json()));
+
+    // loading section
     if (isLoading) {
         return <Loading></Loading>
     }
+
     return (
         <div>
             <h2 className='text-2xl text-center my-5 text-info font-bold'>Users are : {users.length}</h2>
             <div class="overflow-x-auto">
-                <table class="table w-full">
 
-                    <thead>
-                        <tr>
-                            <th></th>
-                            <th>User_Email</th>
-                            <th>Make_Admin</th>
-                            <th>Remove_User</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            users.map((user, index) => <UserRow
-                                key={user._id}
-                                user={user}
-                                index={index}
-                                refetch={refetch}
-                            ></UserRow>)
-                        }
-                    </tbody>
-                </table>
             </div>
         </div>
     );
