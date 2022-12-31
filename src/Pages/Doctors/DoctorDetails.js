fa7d4fb8bf26c032a9b991480cf0c9463103f73d
mehedialmamun21 <mehedialmamuncse@gmail.com>
2023-01-01 01:04:35 +0600

    prescription page edited

--- src/Pages/Doctors/DoctorDetails.js
+++ src/Pages/Doctors/DoctorDetails.js
@@ -16,7 +16,7 @@ const DoctorDetails = () => {
             .then(data => setDoctor(data))
     }, [doctor]);
     return (
-        <section className='h-screen pt-5'>
+        <div className='h-screen my-5'>
 
             <div className='flex justify-center py-5 border-2 border-b-zinc-400 bg-slate-200'>
                 <div className='pr-40'>
@@ -42,18 +42,18 @@ const DoctorDetails = () => {
             <div className='flex border-2'>
                 <div className='w-4/12 px-10 pt-5 border-2 border-r-zinc-300 border-t-white border-l-white border-b-white'>
                     <div>
-                        <p className='bg-success text-white text-center mb-5 py-1 text-xl font-bold'>Symptoms</p>
-                        <ul class="symp m-0 pl-5 py-5 h-full list-disc" data-toggle="tooltip" data-placement="bottom" title="Click to edit."
+                        <p className='bg-slate-500 text-white text-center mb-5 py-1 text-xl font-bold'>Symptoms</p>
+                        <ul class="symp m-0 pl-5 py-5 h-24 bg-gray-200 focus:outline-none" data-toggle="tooltip" data-placement="bottom" title="Click to edit."
                             contenteditable="true">
                         </ul>
-                        <div class="symp_action pb-5 pt-3">
+                        <div class="symp_action pb-8 pt-3">
                             <button class="btn btn-sm px-3 rounded-sm bg-green-500 hover:bg-green-600 border-none text-white save">Save</button>
                             <button class="btn btn-sm px-3 rounded-sm ml-2 bg-red-500 hover:bg-red-600 border-none cancel-btn text-white">Cancel</button>
                         </div>
                     </div>
                     <div>
-                        <p className='bg-success text-white text-center py-1 text-xl font-bold mb-5'>Tests</p>
-                        <ul class="symp m-0 pl-5 py-5 h-full" data-toggle="tooltip" data-placement="bottom" title="Click to edit."
+                        <p className='bg-slate-500 text-white text-center py-1 text-xl font-bold mb-5'>Tests</p>
+                        <ul class="symp m-0 pl-5 py-5 h-24 bg-gray-200 focus:outline-none" data-toggle="tooltip" data-placement="bottom" title="Click to edit."
                             contenteditable="true">
                         </ul>
                         <div class="symp_action pb-5 pt-3">
@@ -61,16 +61,16 @@ const DoctorDetails = () => {
                             <button class="btn btn-sm px-3 ml-2 rounded-sm bg-red-500 hover:bg-red-600 border-none cancel-btn text-white">Cancel</button>
                         </div>
                     </div>
-                    <div>
-                        <p className='bg-success text-white text-center py-1 mb-5 text-xl font-bold'>Advice</p>
-                        <ul class="symp m-0 pl-5 py-5 h-full" data-toggle="tooltip" data-placement="bottom" title="Click to edit."
+                    {/* <div>
+                        <p className='bg-slate-400 text-white text-center py-1 mb-5 text-xl font-bold'>Advice</p>
+                        <ul class="symp m-0 pl-5 py-5 h-24 bg-gray-200 focus:outline-none" data-toggle="tooltip" data-placement="bottom" title="Click to edit."
                             contenteditable="true">
                         </ul>
                         <div class="symp_action pb-5 pt-3">
                             <button class="btn btn-sm px-3 rounded-sm bg-green-500 hover:bg-green-600 border-none text-white save">Save</button>
                             <button class="btn btn-sm px-3 ml-2 rounded-sm bg-red-500 hover:bg-red-600 border-none cancel-btn text-white">Cancel</button>
                         </div>
-                    </div>
+                    </div> */}
                 </div>
 
                 <div className='px-5 py-5 w-full'>
@@ -78,18 +78,23 @@ const DoctorDetails = () => {
                         <img src={prescription2} style={{ width: 90 }} alt="" />
                     </div>
                     <div className='px-6 py-5'>
-                        <ul class="symp m-0 pl-5 py-5 h-full w-full list-disc" data-toggle="tooltip" data-placement="bottom" title="Click here to add medicine."
+                        <ul class="symp m-0 pl-5 py-5 h-60 w-full list-disc bg-gray-200 focus:outline-none" data-toggle="tooltip" data-placement="bottom" title="Click here to add medicine."
                             contenteditable="true">
                         </ul>
-                        <div class="pb-5 pt-3">
+                        <div class="pt-3">
                             <button class="btn btn-sm px-3 rounded-sm bg-green-500 hover:bg-green-600 border-none text-white save">Save</button>
                             <button class="btn btn-sm px-3 rounded-sm ml-2 bg-red-500 hover:bg-red-600 border-none cancel-btn text-white">Cancel</button>
                         </div>
                     </div>
+
+                    <div className='flex justify-end items-end px-6'>
+                        <button className="btn btn-ghost font-semibold bg-gradient-to-r from-secondary to-primary text-white px-2 lg:px-10 hover:scale-105 duration-300 rounded-sm">Print</button>
+                    </div>
+
                 </div>
             </div>
 
-        </section>
+        </div>
     );
 };
 
--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -5,22 +5,24 @@ import chair from '../../assets/images/chair.png';
 
 const Banner = () => {
     return (
-        <section className="hero pt-0 pb-10 lg:py-10 lg:h-[430px] bg-banner-section-backImg">
-            <div className="hero-content flex-col lg:flex-row-reverse">
-                <img src={chair} alt="" className="lg:max-w-sm rounded-lg lg:ml-20" />
-                <div>
-                    <marquee scrollamount="12" className='text-2xl pt-5 lg:pt-0 lg:text-4xl text-zinc-800 font-semibold'>
-                        Your new smile starts here
-                    </marquee>
+        <section>
+            <div className="hero pt-0 pb-10 lg:py-10 lg:h-[430px] bg-banner-section-backImg">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <img src={chair} alt="" className="lg:max-w-sm rounded-lg lg:ml-20" />
+                    <div>
+                        <marquee scrollamount="12" className='text-2xl pt-5 lg:pt-0 lg:text-4xl text-zinc-800 font-semibold'>
+                            Your new smile starts here
+                        </marquee>
 
-                    {/* <p className="py-6 text-zinc-700 w-full lg:w-5/6">Our team is filled with experienced professionals who
+                        {/* <p className="py-6 text-zinc-700 w-full lg:w-5/6">Our team is filled with experienced professionals who
                         are all dedicated to your continued dental health. It is our mission to
                         keep every patient educated and comfortable during their visits, and we
                         will do everything we can to streamline your experience and leave you
                         feeling refreshed and relaxed. Whether you're getting a cleaning, a root
                         canal, or simply require help understanding your insurance, we're here for
                         you!</p> */}
-                    {/* <Link to="/appointment"><PrimaryButton>Get Started</PrimaryButton></Link> */}
+                        {/* <Link to="/appointment"><PrimaryButton>Get Started</PrimaryButton></Link> */}
+                    </div>
                 </div>
             </div>
         </section>
