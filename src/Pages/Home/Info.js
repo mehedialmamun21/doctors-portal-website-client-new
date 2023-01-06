25362ccaeffe8ddd969d2be6fb8a81627760efaa
mehedialmamun21 <mehedialmamuncse@gmail.com>
2023-01-06 21:18:23 +0600

    inpo part icons added

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import InfoCard from './InfoCard';
-import clock from '../../assets/icons/clock.svg';
-import marker from '../../assets/icons/marker.svg';
-import phone from '../../assets/icons/phone.svg';
+
+import { FaMapMarked, FaPhone, FaRegClock } from 'react-icons/fa';
 
 const Info = () => {
     return (
         <div className='grid grid-cols-1 lg:grid-cols-3 gap-2 py-2 lg:py-9 px-5 lg:px-0'>
-            <InfoCard cardTitle="Contact Us" cardDetail="01521-413730" bgClass="bg-secondary" img={phone}></InfoCard>
-            <InfoCard cardTitle="Our Locations" cardDetail="Dental Solution, Dinapur (main branch)" bgClass="bg-accent" img={marker}></InfoCard>
-            <InfoCard cardTitle="Opening Hours" cardDetail="9 A.M (morning) - 10 P.M (night)" bgClass="bg-secondary" img={clock}></InfoCard>
+            <InfoCard cardTitle="Contact Us" cardDetail="01521-413730" bgClass="bg-secondary" icon={<FaPhone />}></InfoCard>
+            <InfoCard cardTitle="Our Locations" cardDetail="Dental Solution, Dinapur (main branch)" bgClass="bg-accent" icon={<FaMapMarked />}></InfoCard>
+            <InfoCard cardTitle="Opening Hours" cardDetail="9 A.M (morning) - 10 P.M (night)" bgClass="bg-secondary" icon={<FaRegClock />}></InfoCard>
         </div>
     );
 };
--- src/Pages/Home/InfoCard.js
+++ src/Pages/Home/InfoCard.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
-const InfoCard = ({ img, cardTitle, cardDetail, bgClass }) => {
+const InfoCard = ({ icon, cardTitle, cardDetail, bgClass }) => {
     return (
         <div className={`card lg:card-side shadow-xl rounded-sm ${bgClass}`}>
-            <figure>
-                <img className='px-5 py-5' src={img} alt="Album" />
-            </figure>
+            <span className='flex justify-center items-center pl-4 text-white text-4xl'>{icon}</span>
             <div className="card-body text-white">
                 <h2 className="card-title">{cardTitle}</h2>
                 <p>{cardDetail}</p>
