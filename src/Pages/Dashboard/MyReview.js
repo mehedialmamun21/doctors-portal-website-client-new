0c139237008d79198ade7469fde02faf4bd97d10
mehedialmamun21 <mehedialmamuncse@gmail.com>
2023-01-07 14:35:37 +0600

    dashboard section review modified

--- src/Pages/Dashboard/MyReview.js
+++ src/Pages/Dashboard/MyReview.js
@@ -33,9 +33,9 @@ const MyReview = () => {
         <div className='addItem mx-5 lg:mx-24 mt-5 lg:mt-5 px-5 lg:px-40 py-5 lg:py-14 shadow-2xl bg-zinc-500'>
             <div className='py-10 bg-white px-5 lg:px-0'>
                 <form className='flex flex-col items-center' onSubmit={handleSubmit(onSubmit)}>
-                    <input className='input input-bordered w-full max-w-xs my-2 rounded-sm shadow-lg bg-slate-200' required placeholder="Give us Ratings (1-5)" type="number" {...register("rating")} />
                     <input className='input input-bordered w-full max-w-xs my-2 rounded-sm shadow-lg bg-slate-200' required placeholder="Your Name" type="text" {...register("name")} />
-                    <textarea className='input input-bordered w-full max-w-xs h-40 my-2 rounded-sm shadow-lg bg-slate-200' required placeholder="Share your valuable feedback with us.." {...register("description")} />
+                    <input className='input input-bordered w-full max-w-xs my-2 rounded-sm shadow-lg bg-slate-200' required placeholder="Give us Ratings  (1-5)" type="number" {...register("rating")} />
+                    <textarea className='input input-bordered w-full max-w-xs h-40 my-2 rounded-sm shadow-lg bg-slate-200' required placeholder="Share your valuable Feedback with us" {...register("description")} />
                     <input className="btn w-full max-w-xs bg-gradient-to-r from-secondary to-primary shadow-lg text-white font-semibold mt-5 border-none rounded-sm hover:scale-105 duration-300" type="submit" value="Add Review" />
                 </form>
             </div>
