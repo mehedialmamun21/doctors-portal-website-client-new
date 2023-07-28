c78cf1700539379ce7ed572b52d6f5ae1f884cc9
mehedialmamun21 <mehedialmamuncse@gmail.com>
2023-07-28 22:06:47 +0600

    chat agent updated with my std.hstu.ac.bd account

--- src/Pages/Home/Chat.js
+++ src/Pages/Home/Chat.js
@@ -7,14 +7,12 @@ export class Chat extends Component {
 
         (function (d, m) {
             var kommunicateSettings =
-                { "appId": "22ed7ecbf226b848b8e5599e330e0da25", "popupWidget": true, "automaticChatOpenOnNavigation": true };
+                { "appId": "2d8cd1f247091cde1b622e5649e91f875", "popupWidget": true, "automaticChatOpenOnNavigation": true };
             var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
             s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
             var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
             window.kommunicate = m; m._globals = kommunicateSettings;
         })(document, window.kommunicate || {});
-
-        /* NOTE : Use web server to view HTML files as real-time update will not work if you directly open the HTML file in the browser. */
 
     }
 
